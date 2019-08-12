d3e1401cc161c595c9a6ffdb0b623be021001d92
valentindumitrescu <47315321+valentindumitrescu@users.noreply.github.com>
2019-08-12 10:16:00 +0300

    Create jobserver.ps1

---
+++ jobserver.ps1
@@ -0,0 +1,106 @@
+
+param(
+	[Object] $p_DbConnServer,
+	[Object] $p_DbConnSqlAuthUser,
+	[Object] $p_DbConnSqlAuthPass,
+	[Object] $p_DbConnDb
+)
+
+# Initialize secondary partition and assign drive letter
+
+Get-Disk |
+
+Where partitionstyle -eq 'raw' |
+
+Initialize-Disk -PartitionStyle MBR -PassThru |
+
+New-Partition -AssignDriveLetter -UseMaximumSize |
+
+Format-Volume -FileSystem NTFS -NewFileSystemLabel "data" -Confirm:$false
+
+# Set Bucharest TimeZone
+
+Set-TimeZone -Name "GTB Standard Time"
+
+# Create local folders
+New-Item -ItemType directory -Path C:\Kits\FintechOS
+New-Item -ItemType directory -Path C:\Temp
+New-Item -ItemType directory -Path F:\UploadEBS
+
+# Add Windows Defender exclusions
+
+Add-MpPreference -ExclusionPath "F:\Sites"
+Add-MpPreference -ExclusionPath "F:\UploadEBS"
+
+# Download and install SSMS
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Invoke-WebRequest -Uri "http://download.microsoft.com/download/3/C/7/3C77BAD3-4E0F-4C6B-84DD-42796815AFF6/SSMS-Setup-ENU.exe" -OutFile "C:\temp\SSMS-Setup-ENU.exe"
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/valentindumitrescu/FintechOS/master/fintechossqlscripts.ps1" -OutFile "C:\temp\fintechossqlscripts.ps1"
+Start-Process -Filepath "C:\temp\SSMS-Setup-ENU.exe" -ArgumentList "/install /quiet /norestart" -wait
+
+
+# Install IIS feature with all needed optional features
+
+Install-WindowsFeature -name Web-Server -IncludeManagementTools
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-ApplicationDevelopment
+Enable-WindowsOptionalFeature -online -FeatureName NetFx4Extended-ASPNET45 -All
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-NetFxExtensibility45 -All
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-ASPNET45 -All
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-ISAPIExtensions -All
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-ApplicationInit -All
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-ISAPIExtensions -All
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-ISAPIFilter -All
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-ASPNET -All
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-ASP -All
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-CGI -All
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-WebServerManagementTools -All
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-ServerSideIncludes -All
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-WebSockets -All
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-ASPNET45 -All
+
+
+
+
+# Download FintechOS files
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Invoke-WebRequest -Uri "https://github.com/valentindumitrescu/FintechOS/raw/ftos_core/7zip.zip" -OutFile "C:\temp\7zip.zip"
+Invoke-WebRequest -Uri "https://github.com/valentindumitrescu/FintechOS/raw/ftos_core/PsExec.zip" -OutFile "C:\kits\psexec.zip"
+Invoke-WebRequest -Uri "https://ftosautodeploystorage.blob.core.windows.net/kits/letsencrypt.zip" -OutFile "C:\kits\letsencrypt.zip"
+Invoke-WebRequest -Uri "https://ftosautodeploystorage.blob.core.windows.net/kits/npp.7.5.9.Installer.x64.exe" -OutFile "C:\kits\npp.7.5.9.Installer.x64.exe"
+Invoke-WebRequest -Uri "https://ftosautodeploystorage.blob.core.windows.net/kits/ChromeSetup.exe" -OutFile "C:\kits\ChromeSetup.exe"
+
+
+# Unzip 7zip to local drive
+Expand-Archive "C:\temp\7zip.zip" -DestinationPath "C:\Temp\"
+Expand-Archive "C:\kits\psexec.zip" -DestinationPath "C:\Kits\"
+Expand-Archive "C:\kits\letsencrypt.zip" -DestinationPath "C:\Kits\"
+
+# Unzip FintechOS
+& C:\kits\npp.7.5.9.Installer.x64.exe /S
+& C:\kits\chromesetup.exe /silent /install
+
+
+$BatFile = "C:\kits\applyscripts.Bat"
+$Code = "C:\Kits\FTOS-CORE\SQL\BasicDbUpgrader.exe -g -s " + $p_DbConnServer + " -d " + $p_DbConnDb + " -u " + $p_DbConnSqlAuthUser + " -p " + $p_DbConnSqlAuthPass + " -c `"C:\Program Files (x86)\Microsoft SQL Server\Client SDK\ODBC\130\Tools\Binn\SQLCMD.EXE`""
+Set-Content -Path $BatFile -Value $code -Encoding ASCII
+
+
+# add letsencrypt SSL certificate and create binding for https
+
+$notificationemail = 'valentin.dumitrescu@fintechos.com'
+
+Set-WebBinding -Name 'Default Web Site' -BindingInformation "*:80:" -PropertyName "HostHeader" -Value $p_fqdn
+
+& c:\Kits\letsencrypt\wacs.exe --target iissite --siteid 1 --emailaddress $notificationemail --accepttos --usedefaulttaskuser
+
+New-WebBinding -name 'Default Web Site' -Protocol https  -HostHeader $p_fqdn -Port 443 -SslFlags 1
+
+
+##################### not final #################################
+$cert = dir cert: -Recurse | Where-Object {$_.FriendlyName -like "*IIS*"}
+
+
+# get-item cert:$cert.thumbprint | new-item -path IIS:\SslBindings\0.0.0.0!443!$p_fqdn
+
+(Get-WebBinding -Name 'Default Web Site' -Port 443 -Protocol https -HostHeader $p_fqdn).AddSslCertificate($cert.Thumbprint, "WebHosting")
+
